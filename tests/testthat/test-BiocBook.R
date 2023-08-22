9dcd1e30c350ae356897a778702026d5dc53e421
js2264 <jacquesserizay@gmail.com>
2023-08-22 23:45:16 +0200

    tests

--- tests/testthat/test-BiocBook.R
+++ tests/testthat/test-BiocBook.R
@@ -1,33 +1,48 @@
-test_that("Methods work", {
+test_that("BiocBook_init work", {
     
-    skip('No PAT set up yet')
+    ## -- Test `BiocBook_init` without user 
+    
+    unlink('BiocBookTest', recursive = TRUE, force = TRUE)
 
-    gert::git_clone('https://github.com/js2264/BiocBook.GRanges')
-
-    expect_no_error(    bb <- BiocBook('BiocBook.GRanges/'))
+    expect_no_error(    bb <- BiocBook_init('BiocBookTest', local = TRUE))
     expect_no_error(    bb)
     expect_no_error(    path(bb))
     expect_no_error(    chapters(bb))
     expect_no_error(    releases(bb))
-    expect_no_error(    BiocBook_versions(bb))
+    expect_no_error(    BiocBook_preview(bb))
+    expect_error(       BiocBook_versions(bb))
 
-    unlink('BiocBook.GRanges', recursive = TRUE, force = TRUE)
-})
-
-test_that("Editing functions work", {
-
-    skip('No PAT set up yet')
-
-    gert::git_clone('https://github.com/js2264/BiocBook.GRanges')
-    
-    expect_no_error(    bb <- BiocBook('BiocBook.GRanges/'))
     expect_invisible(   add_preamble(bb, open = FALSE))
     expect_warning(     add_preamble(bb, open = FALSE))
     expect_invisible(   add_chapter(bb, title = 'Chapter 1', open = FALSE))
     expect_invisible(   add_chapter(bb, title = 'Chapter 1', file = "chapter1.qmd", open = FALSE))
     expect_warning(     add_chapter(bb, title = 'Chapter 1', file = "chapter1.qmd", open = FALSE))
     expect_error(       add_chapter(bb, title = 'Chapter 1', file = "chapter1", open = FALSE))
-    expect_warning(     add_chapter(bb, title = 'Join is an overlap', open = FALSE))
 
-    unlink('BiocBook.GRanges', recursive = TRUE, force = TRUE)
+    unlink('BiocBookTest', recursive = TRUE, force = TRUE)
+    quarto::quarto_preview_stop()
+
+    ## -- Test `BiocBook_init` with user 
+
+    expect_no_error(    
+        BiocBook_init('BiocBookTest', local = TRUE, github_user = 'js2264')
+    )
+    expect_no_error(    bb <- BiocBook('BiocBookTest'))
+    expect_no_error(    path(bb))
+    expect_no_error(    chapters(bb))
+    expect_no_error(    releases(bb))
+    expect_no_error(    BiocBook_preview(bb))
+    expect_error(       BiocBook_versions(bb))
+
+    expect_invisible(   add_preamble(bb, open = FALSE))
+    expect_warning(     add_preamble(bb, open = FALSE))
+    expect_invisible(   add_chapter(bb, title = 'Chapter 1', open = FALSE))
+    expect_invisible(   add_chapter(bb, title = 'Chapter 1', file = "chapter1.qmd", open = FALSE))
+    expect_warning(     add_chapter(bb, title = 'Chapter 1', file = "chapter1.qmd", open = FALSE))
+    expect_error(       add_chapter(bb, title = 'Chapter 1', file = "chapter1", open = FALSE))
+
+    unlink('BiocBookTest', recursive = TRUE, force = TRUE)
+    quarto::quarto_preview_stop()
+
 })
+
